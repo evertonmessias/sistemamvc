ba619f7c1802c5fc76e13563d14eb2db9068882d
evertonmessias <everton.messias@gmail.com>
2020-03-29 18:01:52 -0300

    29-03-2020_18:01:52

--- app/public/funcoes.js
+++ app/public/funcoes.js
@@ -1,50 +1,3 @@
-idd = 0;
-function alterar(x) {
-    idd = 0;
-    $("#anome").val("");
-    $("#atel").val("");
-    $("#aemail").val("");
-    $('tr.linha').css({ 'background-color': '#fff' });
-    $('#alterar').css({ 'display': 'block' });
-    $('#linha' + x).css({ 'background-color': '#ccc' });
-    var nome = $(".tnome" + x).text();
-    var telefone = $(".ttelefone" + x).text();
-    var email = $(".temail" + x).text();
-    $("#anome").val(nome);
-    $("#atel").val(telefone);
-    $("#aemail").val(email);
-    idd = x;
-    $("#botaoalterar").click(function () {        
-        if ($("#anome").val().length < 2 || $("#aemail").val().length < 2 || $("#atel").val().length < 2) {
-            $("#quadro").fadeIn(); $("#mensagem").html("Digite os campos corretamente !");
-            $("#anome").val('').focus(); $("#aemail").val(''); $("#atel").val('');
-            return false;
-        }
-        else {
-            var botaoalterar = $("#botaoalterar").val();
-            var nome = $("#anome").val();
-            var email = $("#aemail").val();
-            var tel = $("#atel").val();
-            $.post("./app/controllers/acao.php", {botaoalterar:botaoalterar , idd: idd, nome: nome, email: email, tel: tel }, function (mostrar) {
-                $("#quadro").fadeIn(); $("#mensagem").html(mostrar);
-            });
-        }
-    });
-}
-function apagar(y) {
-    idd = 0;
-    $('tr.linha').css({ 'background-color': '#fff' });
-    $('#apagar').css({ 'display': 'block' });
-    $('#linha' + y).css({ 'background-color': '#ccc' });
-    idd = y;
-    $("#botaoapagar").click(function () { 
-        var botaoapagar = $("#botaoapagar").val();       
-        $.post("./app/controllers/acao.php", { botaoapagar:botaoapagar,idd: idd }, function (mostrar) {
-            $("#quadro").fadeIn(); $("#mensagem").html(mostrar);
-        });
-    });
-}
-
 $(function () {
 
     $("#quadro").click(function () {
@@ -103,3 +56,49 @@ $(function () {
     });
 
 })
+// *********************
+function alterar(x) {
+    idd = 0;
+    $("#anome").val("");
+    $("#atel").val("");
+    $("#aemail").val("");
+    $('tr.linha').css({ 'background-color': '#fff' });
+    $('#alterar').css({ 'display': 'block' });
+    $('#linha' + x).css({ 'background-color': '#ccc' });
+    var nome = $(".tnome" + x).text();
+    var telefone = $(".ttelefone" + x).text();
+    var email = $(".temail" + x).text();
+    $("#anome").val(nome);
+    $("#atel").val(telefone);
+    $("#aemail").val(email);
+    idd = x;
+    $("#botaoalterar").click(function () {        
+        if ($("#anome").val().length < 2 || $("#aemail").val().length < 2 || $("#atel").val().length < 2) {
+            $("#quadro").fadeIn(); $("#mensagem").html("Digite os campos corretamente !");
+            $("#anome").val('').focus(); $("#aemail").val(''); $("#atel").val('');
+            return false;
+        }
+        else {
+            var botaoalterar = $("#botaoalterar").val();
+            var nome = $("#anome").val();
+            var email = $("#aemail").val();
+            var tel = $("#atel").val();
+            $.post("./app/controllers/acao.php", {botaoalterar:botaoalterar , idd: idd, nome: nome, email: email, tel: tel }, function (mostrar) {
+                $("#quadro").fadeIn(); $("#mensagem").html(mostrar);
+            });
+        }
+    });
+}
+function apagar(y) {
+    idd = 0;
+    $('tr.linha').css({ 'background-color': '#fff' });
+    $('#apagar').css({ 'display': 'block' });
+    $('#linha' + y).css({ 'background-color': '#ccc' });
+    idd = y;
+    $("#botaoapagar").click(function () { 
+        var botaoapagar = $("#botaoapagar").val();       
+        $.post("./app/controllers/acao.php", { botaoapagar:botaoapagar,idd: idd }, function (mostrar) {
+            $("#quadro").fadeIn(); $("#mensagem").html(mostrar);
+        });
+    });
+}
