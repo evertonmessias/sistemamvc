dd85f1ad0651ea2134c7dd15a7d94ab7bf071591
evertonmessias <everton.messias@gmail.com>
2021-01-02 08:51:56 -0300

    02-01-2021_08:51:56

---
+++ app/models/config-example.php
@@ -0,0 +1,9 @@
+<?php
+//definir os campos:
+define('DB_DRIVER','');
+define('DB_HOST','');
+define('DB_DATABASE','');
+define('DB_USERNAME','');
+define('DB_PASSWORD','');
+define('DB_TAB0','users');
+define('GOOGLE',['']);
